70e93169247ad1106810064b88e7cf9950253a72
Bdussart <31310297+Bdussart@users.noreply.github.com>
2023-03-25 17:10:27 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("" + (input.temperature()))
 })
+input.onButtonPressed(Button.AB, function () {
+    music.setVolume(10)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("" + (music.volume()))
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    music.setVolume(127)
+})
